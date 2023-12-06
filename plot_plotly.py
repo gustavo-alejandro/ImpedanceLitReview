8c1d2b7de6ba18c7ac21450fd8b37db23245339f
gustavo-alejandro <gaospinaa@unal.edu.co>
2023-12-06 18:11:05 +1000

    traces update

--- plot_plotly.py
+++ plot_plotly.py
@@ -10,28 +10,21 @@ path = os.getcwd()
 csv_files = glob.glob(os.path.join(path, "*.xlsx"))
 
 df = pd.read_excel(csv_files[0])
-Zr1 = [0, 2, 6, 7, 9]
-Zim1 = [2, 4, 12, 15, 20]
-
-Zr2 = [5, 7, 13, 12, 14]
-Zim2 = [6, 8, 16, 19, 24]
-
 # Create traces
+#visible='legendonly' disables all legends by default so they need to be turned on interactively
 traces=[]
+marker_size=4
 for i in range(0, df.shape[1], 2):
     x_col = list(df[df.columns[i]])
     y_col = list(df[df.columns[i+1]])
-    traces.append(go.Scatter(x=x_col, y=y_col, mode='markers', name=df.columns[i+1]))
-
-
-#trace1 = go.Scatter(x=Zr1, y=Zim1, mode='markers', name='Dataset 1')
-#trace2 = go.Scatter(x=Zr2, y=Zim2, mode='markers', name='Dataset 2')
+    traces.append(go.Scatter(x=x_col, y=y_col, mode='markers', marker=dict(size=marker_size), visible='legendonly',name=df.columns[i+1]))
 
 # Layout
-layout = go.Layout(title='Zr vs Zim Interactive Plot',
+layout = go.Layout(title='Hall-Heroult Impedance Measurements Lit. Review',
                    xaxis=dict(title='Zr'),
-                   yaxis=dict(title='Zim'),
-                   hovermode='closest')
+                   yaxis=dict(title='-Zim'),
+                   hovermode='closest'
+                   )
 
 # Create figure
 fig = go.Figure(data=traces, layout=layout)
